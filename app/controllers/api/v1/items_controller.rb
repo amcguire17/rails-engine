76689a7d8ef6c35d80900f4b0fa0842019de1c32
Amanda McGuire <aruwart001@gmail.com>
2021-09-06 10:15:05 -0600

    Update index method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Merchant.find(params[:merchant_id]).items
+    get_page
+    get_per_page
+    items = Item.get_list(get_page, get_per_page)
     render json: ItemSerializer.new(items)
   end
 end
